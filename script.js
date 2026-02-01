c833099c03c98bd05a6cf3a05bda84834cf3bf70
Subigya Shrestha <subigyashrestha599@gmail.com>
2026-02-01 13:33:05 +0545

    learned function and loop in js

---
+++ script.js
@@ -0,0 +1,51 @@
+let a = 5;
+// console.log(a);
+let fruits = ["apple", "banana", "cherry", 1, 2, 3, true, false];
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+console.log("------------------------------------");
+fruits.forEach(fruit => {
+    console.log(fruit)
+})
+//learning function
+/*4 types
+    with input(argument),with return(return type)
+    without input(argument),with return(return type)
+    with input(argument),without return(return type)
+    without input(argument),without return(return type)
+*/
+
+
+// function sum(num1, num2) {
+//     let num3 = num1 + num2;
+//     return num3;
+// }
+// let b=sum(2,3);
+// console.log(b);
+
+
+
+// function sum() {
+//     let num3 = 5 + 5;
+//     return num3;
+// }
+// let c=sum();
+// console.log(c);
+
+
+
+// function sum(num1, num2) {
+//     let num3 = num1 + num2;
+//     console.log(num3);
+// }
+// console.log(sum('2',3));
+
+
+
+function sum()
+{
+    let num1=5+5;
+    console.log(num3);
+}
+sum();
